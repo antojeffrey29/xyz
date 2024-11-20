4009b5e70bf2d5903f7db46903a5fcd272999cfd
Anto Jeffrey <antojeffrey29@gmail.com>
2024-11-21 00:40:52 +0530

    updates2

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
 }
 
 provider "aws" {
